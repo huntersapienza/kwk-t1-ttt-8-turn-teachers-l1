538534abaea160e3ab96d791f6fed6cd6961ea49
Hunter Sapienza <sapienzahunter@gmail.com>
2018-05-15 01:29:30 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -22,8 +22,8 @@ def input_to_index(input)
 index = (input.to_i - 1)
 end
 
-def move(array, index, value="X")
-  array[index]=value
+def move(board, index, value="X")
+  board[index]=value
   display_board(board)
 end
 
