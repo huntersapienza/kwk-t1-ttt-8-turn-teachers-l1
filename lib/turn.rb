0779512e20d1704fac059d285e9cc9ed0172f551
Hunter Sapienza <sapienzahunter@gmail.com>
2018-05-15 01:13:49 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,3 +6,12 @@ def display_board(board=[" ", " ", " ", " ", " ", " ", " ", " ", " "])
   puts("-----------")
   puts(" #{board[6]} | #{board[7]} | #{board[8]} ")
 end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def position_taken?(board, index)
+   board[index] != " "
+   board[index] == "X" || board[index] == "O"
+end
